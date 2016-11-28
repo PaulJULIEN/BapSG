63429bcbadd9808df1dde5f2e3c27fe07f64f0cc
Paul JULIEN <julien.paul95@gmail.com>
2016-11-28 10:56:54 +0100

    ajout formulaire connexion / inscription + confirmation email

---
+++ inc/header.php
@@ -0,0 +1,58 @@
+<?php
+if(session_status() == PHP_SESSION_NONE){
+    session_start();
+}
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Mon super projet</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/app.css" rel="stylesheet">
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Mon super projet</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <?php if (isset($_SESSION['auth'])): ?>
+                    <li><a href="logout.php">Se déconnecter</a></li>
+                <?php else: ?>  
+                    <li><a href="register.php">S'inscrire</a></li>
+                    <li><a href="login.php">Se connecter</a></li>
+                <?php endif; ?>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
+<div class="container">
+
+    <?php if(isset($_SESSION['flash'])): ?>
+        <?php foreach($_SESSION['flash'] as $type => $message): ?>
+            <div class="alert alert-<?= $type; ?>">
+                <?= $message; ?>
+            </div>
+        <?php endforeach; ?>
+        <?php unset($_SESSION['flash']); ?>
+    <?php endif; ?>
+
---
+++ register.php
@@ -0,0 +1,93 @@
+<?php
+require_once 'inc/functions.php';
+session_start();
+if(!empty($_POST)){
+
+    $errors = array();
+    require_once 'inc/db.php';
+
+    if(empty($_POST['username']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['username'])){
+        $errors['username'] = "Votre pseudo n'est pas valide (alphanumérique)";
+    } else {
+        $req = $pdo->prepare('SELECT id FROM users WHERE username = ?');
+        $req->execute([$_POST['username']]);
+        $user = $req->fetch();
+        if($user){
+            $errors['username'] = 'Ce pseudo est déjà pris';
+        }
+    }
+
+    if(empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        $errors['email'] = "Votre email n'est pas valide";
+    } else {
+        $req = $pdo->prepare('SELECT id FROM users WHERE email = ?');
+        $req->execute([$_POST['email']]);
+        $user = $req->fetch();
+        if($user){
+            $errors['email'] = 'Cet email est déjà utilisé pour un autre compte';
+        }
+    }
+
+    if(empty($_POST['password']) || $_POST['password'] != $_POST['password_confirm']){
+        $errors['password'] = "Vous devez rentrer un mot de passe valide";
+    }
+
+if(empty($errors)){
+
+    $req = $pdo->prepare("INSERT INTO users SET username = ?, password = ?, email = ?, confirmation_token = ?");
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $token = str_random(60);
+    $req->execute([$_POST['username'], $password, $_POST['email'], $token]);
+    $user_id = $pdo->lastInsertId();
+    mail($_POST['email'], 'Confirmation de votre compte', "Afin de valider votre compte merci de cliquer sur ce lien\n\nhttp://local.dev/Lab/Comptes/confirm.php?id=$user_id&token=$token");
+    $_SESSION['flash']['success'] = 'Un email de confirmation vous a été envoyé pour valider votre compte';
+    header('Location: login.php');
+    exit();
+}
+
+
+}
+?>
+
+<?php require 'inc/header.php'; ?>
+
+<h1>S'inscrire</h1>
+
+<?php if(!empty($errors)): ?>
+<div class="alert alert-danger">
+    <p>Vous n'avez pas rempli le formulaire correctement</p>
+    <ul>
+        <?php foreach($errors as $error): ?>
+           <li><?= $error; ?></li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+<?php endif; ?>
+
+<form action="" method="POST">
+
+    <div class="form-group">
+        <label for="">Pseudo</label>
+        <input type="text" name="username" class="form-control"/>
+    </div>
+
+    <div class="form-group">
+        <label for="">Email</label>
+        <input type="text" name="email" class="form-control"/>
+    </div>
+
+    <div class="form-group">
+        <label for="">Mot de passe</label>
+        <input type="password" name="password" class="form-control"/>
+    </div>
+
+    <div class="form-group">
+        <label for="">Confirmez votre mot de passe</label>
+        <input type="password" name="password_confirm" class="form-control"/>
+    </div>
+
+    <button type="submit" class="btn btn-primary">M'inscrire</button>
+
+</form>
+
+<?php require 'inc/footer.php'; ?>
